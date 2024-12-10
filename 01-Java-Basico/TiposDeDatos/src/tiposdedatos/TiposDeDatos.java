227118035db7e2199082a7d22610c1def6451b8b
frishlin <frishlin@gmail.com>
2024-12-09 18:47:59 -0600

    ejercico2 - tipos de datos

---
+++ 01-Java-Basico/TiposDeDatos/src/tiposdedatos/TiposDeDatos.java
@@ -0,0 +1,22 @@
+
+package tiposdedatos;
+
+public class TiposDeDatos {
+
+    public static void main(String[] args) {
+        //tipo de dato + nombre de la variable
+        int edad = 42;
+        String nombre = "Marisol";
+        boolean bandera = true;
+        double estatura = 1.66;
+        
+        System.out.println("Mi nombre es: " + nombre + ", tengo " + edad + " años de edad" + ", mi estatura es " + estatura + " metros y me encuentro en estado " + bandera);
+    
+        // asignando tipos de datos que no corresponden
+        
+        int nombreGato = "Síkis";
+        String estado = false;
+        boolean edadGato = 11.5;
+        double medidaGato = true;
+    }
+}
